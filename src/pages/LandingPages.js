a8dd0be1efc7cfd155f7cc8ef05eb833852586b9
Sinatrio Bimo Wahyudi <44100152+sinatriiobimo@users.noreply.github.com>
2020-07-22 19:12:57 +0700

    testing Button elements

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Button from 'elements/Button';
+
+export default class LandingPages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
