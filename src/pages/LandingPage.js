cdd9162a82bf5c44c2d6011f9e4340871cafef58
Sinatrio Bimo Wahyudi <44100152+sinatriiobimo@users.noreply.github.com>
2020-07-28 21:28:21 +0700

    Adding part testimony and testing star element

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+import Testimony from 'parts/Testimony';
 import landingPage from 'json/landingPage.json';
 
 export default class LandingPages extends Component {
@@ -17,6 +18,7 @@ export default class LandingPages extends Component {
                 <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/>
                 <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked}/>
                 <Categories data={landingPage.categories}/>
+                <Testimony data={landingPage.testimonial}/>
             </>
         )
     }
