aefbe111b54d2e7cfc416ae8cebf12465c92ba60
Sinatrio Bimo Wahyudi <44100152+sinatriiobimo@users.noreply.github.com>
2020-08-02 10:33:17 +0700

    Adding payments and completed on checkout parts

---
+++ src/parts/Checkout/Completed.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import CompletedIllustration from 'assets/images/illustration.png';
+
+export default function Completed() {
+    return (
+        <Fade>
+            <div className="container" style={{marginBottom: 30}}>
+                <div className="row justify-content-center text-center">
+                    <div className="col-4">
+                        <img src={CompletedIllustration} className="img-fluid" alt="complete checkout"/>
+                        <p className="text-gray-500">
+                            We will inform you via email later once the transaction has been accepted
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Fade>
+    )
+}
